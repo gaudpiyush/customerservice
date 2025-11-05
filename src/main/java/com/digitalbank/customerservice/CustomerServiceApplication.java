02d7692e8bf3f0aa72c3f4337f417fe7edd593c5
piyush <piyushgaud.18@gmail.com>
2025-11-05 09:08:30 +0530

    "Added the custom made "Observability" lib and resolved the "spring-security" issue"

--- src/main/java/com/digitalbank/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/digitalbank/customerservice/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.digitalbank.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.digitalbank.customerservice", "com.commons"}) //later should be removed
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
